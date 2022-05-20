926e0bba1c5aa6fccb192407866c21c302e6444d
seolyoung <97827365+Hwangseolyoung@users.noreply.github.com>
2022-05-20 17:47:04 +0900

    add type

--- src/main/java/com/choong/spr/controller/BoardController.java
+++ src/main/java/com/choong/spr/controller/BoardController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.choong.spr.domain.BoardDto;
@@ -24,12 +25,28 @@ public class BoardController {
 	
 	@Autowired
 	private ReplyService replyService;
-
+	
+	// 타입과 키워드가 입력되면 화면에 출력하도록 listBoard()에 담기
+	// defaultValue="" 기본값으로 빈스트링을 넣어주면
+	// 검색하지 않아도 기존 리스트가 출력됨
 	@RequestMapping("list")
-	public void list(Model model) {
-		List<BoardDto> list = service.listBoard();
+	public void list(@RequestParam(name = "keyword", defaultValue = "") String keyword,
+					 @RequestParam(name = "type", defaultValue = "") String type, 
+					 Model model){
+		List<BoardDto> list = service.listBoard(type, keyword);
+		
 		model.addAttribute("boardList", list);
 	}
+	
+	/* 위 코드에 합쳐서 작성함(중복코드 제거)
+	// 위 list 메소드와 동일한 일을 한다면 같이 작성하지만
+	// 각각 다른일을 하므로 분리해서 작성하기
+	@RequestMapping(path="list", params="keyword")
+	public void search(String keyword, Model model) {
+		List<BoardDto> list = service.searchBoard(keyword);
+		model.addAttribute("boardList", list);
+	}
+	*/
 	
 	@GetMapping("insert")
 	public void insert() {
@@ -87,6 +104,8 @@ public class BoardController {
 		
 		return "redirect:/board/list";
 	}
+	
+	
 }
 
 
