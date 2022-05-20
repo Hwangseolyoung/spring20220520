8acab2064029cebc6727cdfb8cb7cce527a003cd
seolyoung <97827365+Hwangseolyoung@users.noreply.github.com>
2022-05-20 17:53:06 +0900

    add choose when

--- src/main/java/com/choong/spr/mapper/BoardMapper.java
+++ src/main/java/com/choong/spr/mapper/BoardMapper.java
@@ -2,11 +2,15 @@ package com.choong.spr.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.choong.spr.domain.BoardDto;
 
 public interface BoardMapper {
-
-	List<BoardDto> selectBoardAll();
+	
+	// 검색 기능 추가로 파라미터에서 받은 키워드 입력
+	// 컴파일시 파라미터 이름이 사라짐으로 @Param에 이름 명시하기(파라미터로 2개이상 받을시)
+	List<BoardDto> selectBoardAll(@Param("type") String type, @Param("keyword") String keyword);
 
 	int insertBoard(BoardDto board);
 
@@ -15,5 +19,7 @@ public interface BoardMapper {
 	int updateBoard(BoardDto dto);
 
 	int deleteBoard(int id);
+	
+	
 
 }
