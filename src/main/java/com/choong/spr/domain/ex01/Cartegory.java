01c9749afd5a769866cea02fcf10f43ff9df1ddb
seolyoung <97827365+Hwangseolyoung@users.noreply.github.com>
2022-05-20 17:49:48 +0900

    category

---
+++ src/main/java/com/choong/spr/domain/ex01/Cartegory.java
@@ -0,0 +1,12 @@
+package com.choong.spr.domain.ex01;
+
+import lombok.Data;
+
+@Data
+public class Cartegory {
+	
+	private String categoryId;
+	private String categoryName;
+	private String description;
+
+}
