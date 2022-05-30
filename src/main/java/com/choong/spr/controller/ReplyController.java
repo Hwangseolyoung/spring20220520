7db7bac31d7e8f53a072ed23b2361c3f5815cc4c
seolyoung0811@gmail.com <seolyoung0811@gmail.com>
2022-05-30 17:39:03 +0900

    add own

--- src/main/java/com/choong/spr/controller/ReplyController.java
+++ src/main/java/com/choong/spr/controller/ReplyController.java
@@ -1,5 +1,6 @@
 package com.choong.spr.controller;
 
+import java.security.Principal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,43 +27,84 @@ public class ReplyController {
 
 	@Autowired
 	private ReplyService service;
-
+	
+	// 회원만 댓글 작성 가능하도록 수정(Principal principal)
+	// Principal : security login 정보가 담겨있다.
 	@PostMapping(path = "insert",  produces = "text/plain;charset=UTF-8")
-	public ResponseEntity<String> insert(ReplyDto dto) {
-
-		boolean success = service.insertReply(dto);
-
-		if (success) {
-			return ResponseEntity.ok("새 댓글이 등록되었습니다.");
+	public ResponseEntity<String> insert(ReplyDto dto, Principal principal) {
+		
+		
+		if (principal == null) { 
+			// 회원이 아니라면 UNAUTHORIZED 오류 401(접근불가)
+			// 페이지 검사-네트워크에 보면 status 401 메세지를 볼 수 있다.
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
 		} else {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("error");
+			// 로그인 회원 아이디 얻어오기
+			dto.setMemberId(principal.getName());
+			
+			/* 위 한줄 풀어쓰기
+			 String memberId = principal.getName();
+			 dto.setMemberId(memberId);
+			 */
+			
+			boolean success = service.insertReply(dto);
+			
+			if (success) {
+				return ResponseEntity.ok("새 댓글이 등록되었습니다.");
+			} else { // 댓글이 등록되지 않으면 상태 메세지 출력 500 Internal Server Error
+				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("error");
+			}
+			
 		}
+		
 
 		
 	}
-
+	
+	// 댓글 쓴 사람만 수정가능하도록 추가
 	@PutMapping(path = "modify", produces = "text/plain;charset=UTF-8")
 	// @RequestBody : json 형식의 객체로 변경해주는 어노테이션
-	public ResponseEntity<String> modify(@RequestBody ReplyDto dto) { 
-		boolean success = service.updateReply(dto);
-
-		if (success) {
-			return ResponseEntity.ok("댓글이 변경되었습니다.");
-		} 
+	public ResponseEntity<String> modify(@RequestBody ReplyDto dto, Principal principal) { 
+		
+		// 회원이 아니라면 UNAUTHORIZED 오류 401(접근불가)
+		// 페이지 검사-네트워크에 보면 status 401 메세지를 볼 수 있다.
+		if (principal == null) {
+			return ResponseEntity.status(401).build();
+	
+		} else {
+			
+			// 로그인 정보 넘겨주기
+			boolean success = service.updateReply(dto, principal);
+			
+			if (success) {
+				return ResponseEntity.ok("댓글이 변경되었습니다.");
+			} 
 			return ResponseEntity.status(500).body("");
+		}
+		
 
 		/*rttr.addAttribute("id", dto.getBoardId());
 		return "redirect:/board/get";*/
 	}
 	
+	// 댓글 쓴 사람만 삭제 가능하도록 추가
 	@DeleteMapping(path = "delete/{id}", produces = "text/plain;charset=UTF-8")
-	public ResponseEntity<String> delete(@PathVariable("id") int id) {
-		boolean success = service.deleteReply(id);
+	public ResponseEntity<String> delete(@PathVariable("id") int id, Principal principal) {
 		
-		if (success) {
-			return ResponseEntity.ok("댓글이 삭제되었습니다.");
+		// 회원이 아니라면 UNAUTHORIZED 오류 401(접근불가)
+		// 페이지 검사-네트워크에 보면 status 401 메세지를 볼 수 있다.
+		if (principal == null) {
+			return ResponseEntity.status(401).build();
 		} else {
-			return ResponseEntity.status(500).body("");
+			
+			boolean success = service.deleteReply(id, principal);
+			
+			if (success) {
+				return ResponseEntity.ok("댓글을 삭제 하였습니다.");
+			} else {
+				return ResponseEntity.status(500).body("");
+			}
+		 
 		}
 		
 		/*rttr.addAttribute("id", dto.getBoardId());
@@ -70,9 +112,16 @@ public class ReplyController {
 	}
 	
 	// json 형식 응답으로 변경
+	// 댓글 작성자만 수정 삭제 가능하도록 추가
 	@GetMapping("list")
-	public List<ReplyDto> list(int boardId) {
-		return service.getReplyByBoardId(boardId);
+	public List<ReplyDto> list(int boardId, Principal principal) {
+		
+		if (principal == null) { // 로그인한 사람이 없으면 boardId만 넣어주기
+			return service.getReplyByBoardId(boardId);
+			
+		} else { // 로그인한 사람이 있으면 파라미터로 boardId와 로그인 정보 넣어주기
+			return service.getReplyWithOwnByBoardId(boardId, principal.getName());
+		}
 
 	}
 }
