0a1ecae531bd76a7f143ed1de007635f544fc1eb
seolyoung0811@gmail.com <seolyoung0811@gmail.com>
2022-05-30 17:39:46 +0900

    remove member

--- src/main/java/com/choong/spr/mapper/ReplyMapper.java
+++ src/main/java/com/choong/spr/mapper/ReplyMapper.java
@@ -2,18 +2,27 @@ package com.choong.spr.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.choong.spr.domain.ReplyDto;
 
 public interface ReplyMapper {
 
 	int insertReply(ReplyDto dto);
-
-	List<ReplyDto> selectAllBoardId(int boardId);
+	
+	// 댓글 작성자만 수정 삭제 가능하도록 추가 
+	List<ReplyDto> selectAllBoardId(@Param("boardId") int boardId, @Param("memberId") String memberId);
 
 	int updateReply(ReplyDto dto);
 
 	int deleteReply(int id);
 
 	void deleteByBoardId(int boardId);
+	
+	// 댓글 쓴 사람만 수정가능하도록 추가
+	ReplyDto selectReplyById(int id);
+	
+	
+	void deleteByMemberId(String memberId);
 
 }
